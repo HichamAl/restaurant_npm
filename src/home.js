b75807520ce13441194ae1a3746ad0357d06d364
HichamAl <h.almakroudi@icloud.com>
2026-01-01 17:25:49 +0100

    add homepage dynamically created using js

---
+++ src/home.js
@@ -0,0 +1,7 @@
+export {headingText, image}
+
+const headingText = document.createElement("h1");
+headingText.innerHTML = 'The best website every YOUKNOW';
+
+const image = document.createElement("img");
+image.setAttribute("href", "https://mnarcastle.ma/wp-content/uploads/2025/08/Safety-in-Morocco.jpg");
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./styles.css";
+import { headingText, image } from "./home";
 
 console.log("test");
+
+const container = document.querySelector("#content");
+container.appendChild(headingText, image);
